043c6d17939fa336d96b0cd3f3a41f998edf6e66
unknown <jame_glove@yahoo.com>
2021-01-03 16:28:42 +0800

    hadeh kelupaan

--- administrator/restrict.js
+++ administrator/restrict.js
@@ -1,35 +1,19 @@
 const Discord = require('discord.js');
-const randomcolor_1 = require("randomcolor");
-const { query } = require("../handlers/DatabaseHandler");
+const fetch = require('node-fetch');
+const querystring = require('querystring');
 const config = require("../config.json")
 
 module.exports = function (client) {
     client.on('message', async message => {
-        if (!message.content.startsWith(config.bot.prefix) || message.author.bot) return;
-        const args = message.content.slice(config.bot.prefix.length).trim().split(/ +/);
-        const command = args.shift().toLowerCase();
-        if (command === 'restrict') {
-            if (!message.member.roles.cache.some(role => (role.name == "Community Manager" || role.name === "Chat Moderators"))) { return message.channel.send("This is reserved for ilyt, sorry!") }
-            if (!args.length) {
-                return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-            }
-            try {
-                var userInput = args.join('').toLowerCase().replace(/\s/g,'_');
-                var reason = args.slice(1).join(" ");
-                var getUser = await query("SELECT * FROM users WHERE username = ?", userInput)
-                query("UPDATE users SET privileges = '2', notes = ? WHERE id = ?", reason, getUser[0].id)
-                let color = randomcolor_1.randomColor();
-                let hex = parseInt(color.replace(/^#/, ''), 16);
-                const embed = new Discord.MessageEmbed()
-                .setTitle("Restrict")
-                .setColor(hex)
-                .setDescription(`User: [` + userInput + `](https://osu.troke.id/u/` + getUser[0].id +`) has been restricted!\nReason : ` + reason +``)
-                message.channel.send(embed)
-                client.channels.cache.get(config.bot.bannedch).send(embed)
-            } catch (ex) {
-                message.channel.send("user doesnt exist");
-                console.log(userInput);
-            }
+        if (message.content.toLowerCase().startsWith(config.bot.prefix + "restrict")) {
+	    if (!message.member.roles.cache.some(role => (role.name == "Community Manager" || role.name === "Moderator"))) { return message.channel.send("This is reserved for ilyt, sorry!") }
+            var msg = message.content;
+            msg = msg.split(config.bot.prefix + "unban ");
+            msg = msg[1];
+	    if (msg === "") { return message.channel.send("Error.") }
+        if (msg === undefined) { return message.channel.send("This is not a valid option.") }
+            var user = await fetch(`${config.bot.domain}/api/v1/fokabotMessage?k=${config.bot.banchokey}&to=%23admin&msg=!restrict+${msg}`).then(response => response.json());
+            message.channel.send("Your commando has been succesfully execute!")
         }
     });
 }
