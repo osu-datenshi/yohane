c5c5b00926a957bb7840e9cfd204ca220131d0a0
unknown <jame_glove@yahoo.com>
2021-01-17 06:42:35 +0800

    test 123

--- administrator/unrestrict.js
+++ administrator/unrestrict.js
@@ -7,11 +7,10 @@ module.exports = function (client) {
     client.on('message', async message => {
         if (message.content.toLowerCase().startsWith(config.bot.prefix + "unrestrict")) {
         if (!message.member.roles.cache.some(role => (role.name == "Community Manager" || role.name === "Moderator"))) { return message.channel.send("This is reserved for ilyt, sorry!") }
-        if (msg === "") { return message.channel.send("Error.") }
-        if (msg === undefined) { return message.channel.send("This is not a valid option.") }
             var msg = message.content;
             msg = msg.split(config.bot.prefix + "unrestrict ");
             msg = msg[1];
+            if (msg === "") { return message.channel.send("Error.") }
             var user = await fetch(`${config.bot.domain}/api/v1/fokabotMessage?k=${config.bot.banchokey}&to=%23admin&msg=!unrestrict+${msg}`).then(response => response.json());
             message.channel.send("Your commando has been succesfully execute!")
         }
