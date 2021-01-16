9d6b9718a99b6bc8fa162f31cc7d96194f5c2eaf
unknown <jame_glove@yahoo.com>
2021-01-17 06:39:39 +0800

    testing command

--- administrator/unban.js
+++ administrator/unban.js
@@ -6,14 +6,14 @@ const config = require("../config.json")
 module.exports = function (client) {
     client.on('message', async message => {
         if (message.content.toLowerCase().startsWith(config.bot.prefix + "unban")) {
-	    if (!message.member.roles.cache.some(role => (role.name == "Community Manager" || role.name === "Chat Moderators"))) { return message.channel.send("This is reserved for ilyt, sorry!") }
-            var msg = message.content;
-            msg = msg.split(config.bot.prefix + "unban ");
-            msg = msg[1];
-	    if (msg === "") { return message.channel.send("Error.") }
-        if (msg === undefined) { return message.channel.send("This is not a valid option.") }
-            var user = await fetch(`https://c.datenshi.xyz/api/v1/fokabotMessage?k=${config.bot.banchokey}&to=%23admin&msg=!unban+${msg}`).then(response => response.json());
-            message.channel.send("Your commando has been succesfully execute!")
-        }
+            if (!message.member.roles.cache.some(role => (role.name == "Community Manager" || role.name === "Moderator"))) { return message.channel.send("This is reserved for ilyt, sorry!") }
+            if (msg === "") { return message.channel.send("Error.") }
+            if (msg === undefined) { return message.channel.send("This is not a valid option.") }
+                var msg = message.content;
+                msg = msg.split(config.bot.prefix + "unban ");
+                msg = msg[1];
+                var user = await fetch(`${config.bot.domain}/api/v1/fokabotMessage?k=${config.bot.banchokey}&to=%23admin&msg=!unban+${msg}`).then(response => response.json());
+                message.channel.send("Your commando has been succesfully execute!")
+            }
     });
 }
