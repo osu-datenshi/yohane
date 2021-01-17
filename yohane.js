e235b4381596fa1632eb2e0e8d38dd00d765a8f4
Ahmad Yaqdhan <42838410+Mednoob@users.noreply.github.com>
2021-01-17 20:24:47 +0900

    Update yohane.js

--- yohane.js
+++ yohane.js
@@ -13,7 +13,7 @@ global.naughtyWords = config.blacklisted_words
 
 client.commands = new Discord.Collection() //having a command handler is fcking better than register new command manually using require
 client.on("message", (message) => {
-    require("fs").readdirSync("checkers").forEach(ev => require(`./checkers/${ev}`)(client, message))
+    require("fs").readdirSync("checkers").filter(ev => ev.endsWith(".js")).forEach(ev => require(`./checkers/${ev}`)(client, message))
 })
 require("./handlers/commandHandler")(client)
 require("./handlers/eventHandler")(client)
