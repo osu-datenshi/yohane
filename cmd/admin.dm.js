b94e2d1110555026c151c63d2e9cf1eaf53d8c6d
unknown <jame_glove@yahoo.com>
2021-02-26 20:37:34 +0800

    Recent done

--- cmd/admin.dm.js
+++ cmd/admin.dm.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const randomcolor_1 = require("randomcolor");
 
 module.exports = {
-    name: "dm",
+    name: "whitelist",
 
     /**
      * @param {import("discord.js").Client} client 
@@ -23,7 +23,7 @@ module.exports = {
 		.setColor(hex)
 		.setDescription("Your IP Address has been whitelisted, now you can register on our website!\nPlease make sure to read the rules before you start registering on our website!\nif we found you breaking our rules, we can do ban instantly for you!\n\n[Register](https://osu.troke.id/register) | [Rules](https://osu.troke.id/doc/rules) | [Get Member Roles](https://osu.troke.id/discordtokens)")
         mention.send(embed);
-        message.channel.send(`${message.author} You have sent your message to ${mention}`);
+        message.channel.send(`${message.author} Sukses!`);
     }
 
 }
--- cmd/recent.js
+++ cmd/recent.js
@@ -5,18 +5,18 @@ const { pool } = require("../handlers/databaseHandler");
 async function getUserDaten(dcID) {
     return new Promise((resolve, reject) => {
         pool.query(
-            "SELECT users.username, discord_tokens.userid FROM users INNER JOIN discord_tokens ON users.id=discord_tokens.userid WHERE discord_tokens.discord_id = ?",
+            "SELECT * FROM users INNER JOIN discord_tokens ON users.id=discord_tokens.userid WHERE discord_tokens.discord_id = ?",
             [dcID],
             (err, result) => {
             return err ? reject(err) : resolve(result);
         });
     });
 }
-async function getUserRecentDaten(uid) {
+async function getUserRecentDaten(uid, mode) {
     return new Promise((resolve, reject) => {
         pool.query(
-            "SELECT * FROM scores_master INNER JOIN beatmaps ON scores_master.beatmap_md5 = beatmaps.beatmap_md5 WHERE userid = ? AND special_mode = 1 ORDER BY time DESC",
-            [uid],
+            `SELECT * FROM scores_master INNER JOIN beatmaps ON scores_master.beatmap_md5 = beatmaps.beatmap_md5 WHERE userid = ? AND special_mode = ? ORDER BY time DESC`,
+            [uid, mode],
             (err, result) => {
             return err ? reject(err) : resolve(result);
         });
@@ -33,25 +33,27 @@ const subcommands = {
     vanilla: async (client, message, args) => {
         const msg = args.join(" ")
         try {
-            var user = await query("SELECT * FROM users WHERE username = ?", msg);
-            // Special mode = 0 (vanilla) 1 (relax) 2 (scorev2)
-            var recentScore = await query("SELECT * FROM scores_master INNER JOIN beatmaps ON scores_master.beatmap_md5 = beatmaps.beatmap_md5 WHERE userid = ? AND special_mode = 0 ORDER BY time DESC;", user[0].id)
+            var AmbilID = message.author.id;
+            var user = await getUserDaten(AmbilID);
+            // Score Mode = 0 (Vanilla), 1 (Relax)
+            var recentScore = await getUserRecentDaten(user[0].userid, 0);
             let color = randomcolor_1.randomColor();
             let hex = parseInt(color.replace(/^#/, ''), 16);
+            const formatNumber = (num) => (num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,'));
 
             const embed = new Discord.MessageEmbed()
-            .setTitle("Recent Score For " + msg)
-            .setURL("https://osu.troke.id/u/" + recentScore[0].userid)
+            .setTitle("Recent Score For " + user[0].username)
+            .setURL("https://osu.troke.id/u/" + user[0].userid)
             .setColor(hex)
-            .setDescription(recentScore[0].song_name)
-		    .setThumbnail("https://a.troke.id/" + user[0].id)
+            .setDescription(recentScore[0].artist + " - " + recentScore[0].title + " [" + recentScore[0].difficulty_name + "]")
+		    .setThumbnail("https://a.troke.id/" + user[0].userid)
             .setImage("https://assets.ppy.sh/beatmaps/" + recentScore[0].beatmapset_id + "/covers/cover.jpg")
             .addField("PP:", recentScore[0].pp)
-            .addField("Score:", recentScore[0].score)
+            .addField("Score:", formatNumber(recentScore[0].score))
             .addField("Accuracy:", Math.round(recentScore[0].accuracy) + "%")
             message.channel.send(embed);
         } catch (ex) {
-            message.channel.send("User doesn't exist");
+            message.channel.send("Maaf user tidak dapat ditemukan, silahkan link account jika belum ada!");
         }
     },
 
@@ -64,11 +66,11 @@ const subcommands = {
         try {
             var AmbilID = message.author.id;
             var user = await getUserDaten(AmbilID);
-            // Special mode = 0 (vanilla) 1 (relax) 2 (scorev2)
-            var recentScore = await getUserRecentDaten(user[0].userid);
+            // Score Mode = 0 (Vanilla), 1 (Relax)
+            var recentScore = await getUserRecentDaten(user[0].userid, 1);
             let color = randomcolor_1.randomColor();
             let hex = parseInt(color.replace(/^#/, ''), 16);
-            
+            const formatNumber = (num) => (num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,'));
             const embed = new Discord.MessageEmbed()
             .setTitle("Recent Score For " + user[0].username)
             .setURL("https://osu.troke.id/rx/u/" + user[0].userid)
@@ -77,12 +79,12 @@ const subcommands = {
             .setThumbnail("https://a.troke.id/" + user[0].userid)
             .setImage("https://assets.ppy.sh/beatmaps/" + recentScore[0].beatmapset_id + "/covers/cover.jpg")
             .addField("PP:", recentScore[0].pp)
-            .addField("Score:", recentScore[0].score)
+            .addField("Score:", formatNumber(recentScore[0].score))
             .addField("Accuracy:", Math.round(recentScore[0].accuracy) + "%")
             message.channel.send(embed);
         } catch (ex) {
             console.log(ex);
-            message.channel.send("User doesn't exist");
+            message.channel.send("Maaf user tidak dapat ditemukan, silahkan link account jika belum ada!");
         }
     }
 
