2728c5f6aee63b4689b66bdb96b219ae5923afed
unknown <jame_glove@yahoo.com>
2021-03-03 06:56:06 +0800

    stats vanilla clear

--- cmd/recent.js
+++ cmd/recent.js
@@ -40,17 +40,43 @@ const subcommands = {
             let color = randomcolor_1.randomColor();
             let hex = parseInt(color.replace(/^#/, ''), 16);
             const formatNumber = (num) => (num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,'));
-
+            var waktunya = recentScore[0].time;
+            var kali = new Date(waktunya*1000);
+            var mode;
+            var playMode = recentScore[0].play_mode;
+    
+            switch (playMode) {
+                case 0:
+                    mode = "<:modeosu:800724455579713536>";
+                    break;
+                case 1:
+                    mode = "<:modetaiko:800724456070447196>";
+                    break;
+                case 2:
+                    mode = "<:modefruits:800724454980190228>";
+                    break;
+                case 3:
+                    mode = "<:modemania:800724455126335538>";
+                    break;
+                default:
+                    mode = "Unkwon";
+            }
             const embed = new Discord.MessageEmbed()
             .setTitle("Recent Score For " + user[0].username)
             .setURL("https://osu.troke.id/u/" + user[0].userid)
             .setColor(hex)
-            .setDescription(recentScore[0].artist + " - " + recentScore[0].title + " [" + recentScore[0].difficulty_name + "]")
 		    .setThumbnail("https://a.troke.id/" + user[0].userid)
             .setImage("https://assets.ppy.sh/beatmaps/" + recentScore[0].beatmapset_id + "/covers/cover.jpg")
-            .addField("PP:", recentScore[0].pp)
-            .addField("Score:", formatNumber(recentScore[0].score))
-            .addField("Accuracy:", Math.round(recentScore[0].accuracy) + "%")
+            .setTimestamp(kali.toGMTString())
+            .setFooter(user[0].username, "https://cdn.troke.id/static/logos/text-white.png")
+            .addField("PP:", formatNumber(Math.round(0.0 + recentScore[0].pp)), true)
+            .addField("Score:", formatNumber(recentScore[0].score), true)
+            .addField("Accuracy:", Math.round(0.0 + recentScore[0].accuracy) + "%", true)
+            .addField("Mode:", mode)
+            .addField("Artist:", recentScore[0].artist)
+            .addField("Title:", recentScore[0].title)
+            .addField("Difficulty:", recentScore[0].difficulty_name)
+            .addField("Download Beatmap:", "[osu!](https://osu.ppy.sh/d/" + recentScore[0].beatmapset_id +") • [beatconnect](https://beatconnect.io/b/" + recentScore[0].beatmapset_id +") • [datenshi](https://osu.troke.id/d/" + recentScore[0].beatmapset_id +")")
             message.channel.send(embed);
         } catch (ex) {
             message.channel.send("Maaf user tidak dapat ditemukan, silahkan link account jika belum ada!");
@@ -71,16 +97,43 @@ const subcommands = {
             let color = randomcolor_1.randomColor();
             let hex = parseInt(color.replace(/^#/, ''), 16);
             const formatNumber = (num) => (num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,'));
+            var waktunya = recentScore[0].time;
+            var kali = new Date(waktunya*1000);
+            var mode;
+            var playMode = recentScore[0].play_mode;
+    
+            switch (playMode) {
+                case 0:
+                    mode = "<:modeosu:800724455579713536>";
+                    break;
+                case 1:
+                    mode = "<:modetaiko:800724456070447196>";
+                    break;
+                case 2:
+                    mode = "<:modefruits:800724454980190228>";
+                    break;
+                case 3:
+                    mode = "<:modemania:800724455126335538>";
+                    break;
+                default:
+                    mode = "Unkwon";
+            }
             const embed = new Discord.MessageEmbed()
             .setTitle("Recent Score For " + user[0].username)
             .setURL("https://osu.troke.id/rx/u/" + user[0].userid)
             .setColor(hex)
-            .setDescription(recentScore[0].artist + " - " + recentScore[0].title + " [" + recentScore[0].difficulty_name + "]")
             .setThumbnail("https://a.troke.id/" + user[0].userid)
             .setImage("https://assets.ppy.sh/beatmaps/" + recentScore[0].beatmapset_id + "/covers/cover.jpg")
-            .addField("PP:", recentScore[0].pp)
-            .addField("Score:", formatNumber(recentScore[0].score))
-            .addField("Accuracy:", Math.round(recentScore[0].accuracy) + "%")
+            .setTimestamp(kali.toGMTString())
+            .setFooter(user[0].username, "https://cdn.troke.id/static/logos/text-white.png")
+            .addField("PP:", formatNumber(Math.round(0.0 + recentScore[0].pp)), true)
+            .addField("Score:", formatNumber(recentScore[0].score), true)
+            .addField("Accuracy:", Math.round(0.0 + recentScore[0].accuracy) + "%", true)
+            .addField("Mode:", mode)
+            .addField("Artist:", recentScore[0].artist)
+            .addField("Title:", recentScore[0].title)
+            .addField("Difficulty:", recentScore[0].difficulty_name)
+            .addField("Download Beatmap:", "[osu!](https://osu.ppy.sh/d/" + recentScore[0].beatmapset_id +") • [beatconnect](https://beatconnect.io/b/" + recentScore[0].beatmapset_id +") • [datenshi](https://osu.troke.id/d/" + recentScore[0].beatmapset_id +")")
             message.channel.send(embed);
         } catch (ex) {
             console.log(ex);
