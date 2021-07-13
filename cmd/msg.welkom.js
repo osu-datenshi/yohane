15f44168f38071679b249602647cce2a55c2b3ea
unknown <jame_glove@yahoo.com>
2021-07-13 16:31:22 +0800

    Revisi dari <@433905134259535893>

--- cmd/msg.welkom.js
+++ cmd/msg.welkom.js
@@ -34,7 +34,7 @@ module.exports = {
 		const welcome = new Discord.MessageEmbed()
 		.setAuthor("ABOUT US", "https://raw.githubusercontent.com/osu-datenshi/assets/master/logo.png", "https://datenshi.pw")
 		.setColor(hex)
-		.setDescription("DATENSHI is a multi-gaming community server founded in 12 April 2020 Indonesia. We have two games community server that we served to the players, there are osu! and Minecraft.\n\nosu! is the first and the largest one community server in Indonesia, we are providing the home for players who dont have any 'place' to take shelter\n\nMinecraft, this is the best solution for people who tired for clicking circles in osu! and trying to build or survive in the game.\n\nOur community is filled with amazing people and very active members that make DATENSHI is more very interesting, don't hesitate to join us!\n\n***Please click the buttons to get more informations about our server!***")
+		.setDescription("DATENSHI is a multi-gaming community server founded in 12 April 2020. We have two games that our community served to their players. There are osu! and Minecraft.\n\nosu! is the first and the largest one community server in Indonesia, we are providing home for players who don't have any 'place' to take shelter\n\nMinecraft, is the best solution for people who tired of clicking circles in osu! and wanted to build or survive in a game.\n\nOur community are filled with amazing people and also the members are very active, that's why DATENSHI is more interesting place, don't hesitate to join us!\n\n***Please click the buttons to get more informations about our server!***")
 		.setImage("https://cdn.discordapp.com/attachments/728581754398572546/849187512689360906/DATENSHI_BANNER.png")
 
 		message.channel.send({ embed: welcome, component: row})
