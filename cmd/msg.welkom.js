ac6801e94b506563eaf7e35de40ddb3b74b637c6
unknown <jame_glove@yahoo.com>
2021-07-13 13:52:20 +0800

    Add buttons

--- cmd/msg.welkom.js
+++ cmd/msg.welkom.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const { MessageButton, MessageActionRow, ButtonCollector } = require('discord-buttons');
 
 module.exports = {
     name: "welkom",
@@ -12,48 +13,30 @@ module.exports = {
         if (message.member.id != client.config.bot.owner_id) return message.channel.send("This is reserved for ilyt, sorry!");
 		let hex = 10181046;
 
-		const bannerWelcome = new (Discord.MessageEmbed)().setColor(hex).setImage("https://cdn.discordapp.com/attachments/728581754398572546/849187512689360906/DATENSHI_BANNER.png")
+		let button = new MessageButton()
+		.setStyle('url')
+		.setURL('https://datenshi.pw/osu-information')
+		.setLabel('osu! server')
+	  
+	  	let button2 = new MessageButton()
+		.setStyle('url')
+		.setURL('https://mc.datenshi.pw/')
+		.setLabel('Minecraft server')
+
+		let button3 = new MessageButton()
+		.setStyle('url')
+		.setURL('https://datenshi.pw')
+		.setLabel('Portal')
+	  
+	 	let row = new MessageActionRow()
+		.addComponents(button3, button, button2);
 
 		const welcome = new Discord.MessageEmbed()
 		.setAuthor("ABOUT US", "https://raw.githubusercontent.com/osu-datenshi/assets/master/logo.png", "https://datenshi.pw")
 		.setColor(hex)
-		.setDescription("DATENSHI is a multi-gaming community server founded in 12 April 2020 Indonesia. We have two games community server that we served to the players, there are osu! and Minecraft.\n\nosu! is the first and the largest one community server in Indonesia, we are providing the home for players who dont have any 'place' to take shelter\n\nMinecraft, this is the best solution for people who tired for clicking circles in osu! and trying to build or survive in the game.\n\nOur community is filled with amazing people and very active members that make DATENSHI is more very interesting, don't hesitate to join us!")
+		.setDescription("DATENSHI is a multi-gaming community server founded in 12 April 2020 Indonesia. We have two games community server that we served to the players, there are osu! and Minecraft.\n\nosu! is the first and the largest one community server in Indonesia, we are providing the home for players who dont have any 'place' to take shelter\n\nMinecraft, this is the best solution for people who tired for clicking circles in osu! and trying to build or survive in the game.\n\nOur community is filled with amazing people and very active members that make DATENSHI is more very interesting, don't hesitate to join us!\n\n***Please click the buttons to get more informations about our server!***")
+		.setImage("https://cdn.discordapp.com/attachments/728581754398572546/849187512689360906/DATENSHI_BANNER.png")
 
-		const bannerosu = new (Discord.MessageEmbed)().setColor(hex).setImage("https://cdn.discordapp.com/attachments/728581754398572546/849194673507663933/osu.png")
-
-		const osu = new Discord.MessageEmbed()
-		.setAuthor("osu! server", "https://raw.githubusercontent.com/osu-datenshi/assets/master/logo.png", "https://osu.datenshi.pw")
-		.setColor(hex)
-		.setDescription("If you are osu! player, please read this\n")
-		.addField("Invite Code", "" + client.config.bot.invitecode + "Please use this code for registration!")
-		.addField("Connecting Guide", "You can go to the [website](https://osu.datenshi.pw/doc/1) for the tutorial")
-		.addField("Help Assistant", "If you need some help, go to this channel <#853228318400708649>")
-
-		const bannerminecraft = new (Discord.MessageEmbed)().setColor(hex).setImage("https://cdn.discordapp.com/attachments/728581754398572546/849196032486015046/minecraft.png")
-
-		const minecraft = new Discord.MessageEmbed()
-		.setAuthor("Minecraft server", "https://raw.githubusercontent.com/osu-datenshi/assets/master/logo.png", "https://datenshi.pw")
-		.setColor(hex)
-		.setDescription("If you are Minecraft player, please read this\n")
-		.addField("Server IP", "`play.datenshi.pw`")
-		.addField("Version", "1.8-latest")
-		.addField("Features", "- Shop\n- Discord Integration\n- Survival Mode\n- Gacha Crates\n- Vote Rewards\nMuch more..")
-		.addField("Help Assistant", "If you need some help, go to this channel <#853236312466522142>")
-
-		const bannerlinks = new (Discord.MessageEmbed)().setColor(hex).setImage("https://cdn.discordapp.com/attachments/728581754398572546/849197933714341888/links_1.png")
-
-		const linksbro = new Discord.MessageEmbed()
-		.setAuthor("LINKS", "https://raw.githubusercontent.com/osu-datenshi/assets/master/logo.png", "https://datenshi.pw")
-		.setColor(hex)
-		.setDescription("- [Website](https://datenshi.pw)\n- [osu! Website](https://osu.datenshi.pw)\n- [osu! Documentation](https://osu.datenshi.pw/doc)\n- [osu! Register](https://osu.datenshi.pw/register)\n- [GitHub](https://github.com/osu-datenshi)\n- [GitLab](https://gitlab.com/osudatenshi)\n- [Facebook Page](https://www.facebook.com/datenshicommunity)\n- [Facebook Group](https://www.facebok.com/groups/osu.datenshi)\n- [Youtube](https://www.youtube.com/channel/UCKwyGpWAD17sVpKwlRDhisw)\n- [Donate](https://datenshi.pw/donations)")
-
-		message.channel.send(bannerWelcome)
-		message.channel.send(welcome)
-		message.channel.send(bannerosu)
-		message.channel.send(osu)
-		message.channel.send(bannerminecraft)
-		message.channel.send(minecraft)
-		message.channel.send(bannerlinks)
-		message.channel.send(linksbro)
+		message.channel.send({ embed: welcome, component: row})
     }
 }
--- yohane.js
+++ yohane.js
@@ -3,6 +3,8 @@ require("events").EventEmitter.prototype._maxListeners = 100
 const config = require("./config.json")
 const Discord = require("discord.js")
 const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"]})
+const disbut = require('discord-buttons');
+disbut(client);
 
 client.config = config
 
